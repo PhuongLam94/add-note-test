2019e89b8bbc25c69490ffffd8c86e4730bd2689
Phuong Lam <phuonglam@kms-technology.com>
2019-04-17 21:05:51 +0700

    add login verification

--- src/main/java/com/pl/pages/MyNotePage.java
+++ src/main/java/com/pl/pages/MyNotePage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyNotePage extends BasePage {
     public MyNotePage(WebDriver driver) {
         super(driver);
@@ -12,7 +15,19 @@ public class MyNotePage extends BasePage {
     @FindBy(css = "button[onclick='App.showAddNotePage();']")
     private WebElement addNoteBtn;
 
+    @FindBy(css = "div#my-notes-page h2")
+    private WebElement titleTxt;
+
+    @FindBy(className = "list-group-item-heading")
+    private List<WebElement> lstGroupItemHead;
+
+    @FindBy(className = "list-group-item-text")
+    private List<WebElement> lstGroupItemText;
+
     public void clickAddNoteBtn(){
         addNoteBtn.click();
+    }
+    public  String getTitle(){
+        return titleTxt.getText();
     }
 }
--- src/test/java/com/pl/tests/LoginTest.java
+++ src/test/java/com/pl/tests/LoginTest.java
@@ -3,11 +3,14 @@ package com.pl.tests;
 import com.pl.data.Constant;
 import com.pl.pages.HomePage;
 import com.pl.pages.LoginPage;
+import com.pl.pages.MyNotePage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Map;
 
 public class LoginTest extends BaseTest{
 
@@ -20,5 +23,8 @@ public class LoginTest extends BaseTest{
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(Constant.DEFAULT_USERNAME, Constant.DEFAULT_PASSWORD);
+
+        MyNotePage myNotePage = new MyNotePage(driver);
+        assert myNotePage.getTitle() == "My Notes";
     }
 }
